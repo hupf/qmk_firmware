23d3c82c8552adf0987b7b83418de7e8fcbae0d2
Mathis Hofer <mathis@fsfe.org>
2021-12-31 14:39:21 +0100

    Add tap and hold Umlauts

--- keyboards/kbdfans/kbd67/rev2/keymaps/hupf/keymap.c
+++ keyboards/kbdfans/kbd67/rev2/keymaps/hupf/keymap.c
@@ -26,7 +26,10 @@ enum custom_keycodes {
   ISO_LBRC,
   ISO_RBRC,
   ISO_2,
-  ISO_MINS
+  ISO_MINS,
+  DUAL_A,
+  DUAL_O,
+  DUAL_U
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -45,8 +48,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
 [0] = LAYOUT_65_ansi(
   KC_GESC, KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_GRV, \
-  KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Z,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, \
-  KC_CAPS, KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT, KC_PGUP,  \
+  KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Z,   DUAL_U,   KC_I,    DUAL_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL, \
+  KC_CAPS, DUAL_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT, KC_PGUP, \
   KC_LSFT, KC_Y,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT /*MT(KC_RSFT, KC_PGUP)*/, KC_UP,   KC_PGDN, \
   KC_LCTL, KC_LGUI, KC_LALT,                KC_SPC,                          KC_RALT, MO(2),   TG(3)/*KC_RCTL*/, KC_LEFT, KC_DOWN, KC_RGHT),
 
@@ -125,6 +128,24 @@ void send_custom(keyrecord_t *record, char* base, char* shifted, char* altered, 
     }
 }
 
+static uint16_t tap_hold_timer;
+static uint8_t tap_hold_mod_state;
+void send_custom_tap_hold(keyrecord_t *record, char* tap, char* hold) {
+    if(record->event.pressed) {
+        tap_hold_timer = timer_read();
+        tap_hold_mod_state = get_mods();
+        send_string(tap); // Write out tap character right away
+      } else {
+        if (timer_elapsed(tap_hold_timer) > TAPPING_TERM) {
+            uint8_t mod_state = get_mods();
+            set_mods(tap_hold_mod_state); // Restore to initially pressed mods to behave more reliably
+            send_string(SS_TAP(X_BSPC)); // Delete initially tapped character
+            send_string(hold); // ...and replace it with the hold character
+            set_mods(mod_state);
+        }
+      }
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
   case ISO_AUML:
@@ -197,6 +218,27 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         SS_LSFT(SS_TAP(X_MINS)), // _ (default)
         SS_TAP(X_QUOT), // '
         KC_NO
+      );
+      break;
+  case DUAL_A:
+      send_custom_tap_hold(
+        record,
+        SS_TAP(X_A), // a (tap)
+        SS_RALT(SS_TAP(X_A)) // ä (hold)
+      );
+      break;
+  case DUAL_O:
+      send_custom_tap_hold(
+        record,
+        SS_TAP(X_O), // o (tap)
+        SS_RALT(SS_TAP(X_O)) // ö (hold)
+      );
+      break;
+  case DUAL_U:
+      send_custom_tap_hold(
+        record,
+        SS_TAP(X_U), // u (tap)
+        SS_RALT(SS_TAP(X_U)) // ü (hold)
       );
       break;
   }
